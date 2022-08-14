bcbcb858f52ac97b3b8b2c435d65642eafc02c3f
vovchikSev <vovchik@programist.ru>
2022-08-14 23:07:14 +0300

    из дома

--- Program.cs
+++ Program.cs
@@ -1,158 +1,102 @@
 ﻿using System;
-namespace project01
+
+public class Program
 {
-    public class Program
+
+    private static string GetTypeInfo(string value) { return "Переменная является строкой"; }
+    private static string GetTypeInfo(int value) { return "Переменная целочисленного типа"; }
+    private static string GetTypeInfo(double value) { return "Переменная - число с плавающей точкой"; }
+    private static string GetTypeInfo(char value) { return "Переменная является символом"; }
+
+    private static void Print(string symb, string count, out string result)
     {
-        private static long Fact(int n)
-        {
-            if (n == 0)
-                return 1;
-            else
-                return n * Fact(n - 1);
-        }
-        public static void Main_()
-        {
-            string num1 = Console.ReadLine();
-            string num2 = Console.ReadLine();
-            string fullNum = num1 + num2;
-            char unique = ' ';
-            for (int index = 0; index < fullNum.Length - 1; index++)
-            {
-                bool flag = true;
-                string slice = fullNum.Substring(index + 1);
-                for (int index2 = index + 1; index2 < slice.Length; index2++)
-                {
-                    if (slice[index2] == fullNum[index])
-                    {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag)
-                {
-                    unique = fullNum[index];
-                    break;
-                }
-            }
-            Console.WriteLine($"Уникально число {unique}");
-
-            Console.ReadLine();
-        }
-        public static void Main__()
-        {
-            string line = Console.ReadLine();
-            string[] array = line.Split(' ');
-            int[] numbers = new int[array.Length];
-            for (int i = 0; i < array.Length; i++)
-                numbers[i] = int.Parse(array[i]);
-
-            int summ = 0;
-            foreach (int value in numbers)
-            {
-                if (value % 2 == 0)
-                    summ += value;
-            }
-
-            Console.WriteLine(summ);
-            Console.ReadLine();
-        }
-        public static void Main_1()
-        {
-            string[] line = Console.ReadLine().Split(" ");
-            int min = Convert.ToInt32(line[0]);
-            int max = Convert.ToInt32(line[0]);
-            foreach (string s in line)
-            {
-                int value = Convert.ToInt32(s);
-                if (value > max)
-                    max = value;
-                if (value < min)
-                    min = value;
-
-            }
-            Console.WriteLine($"Наименьшее значение = {min}");
-            Console.WriteLine($"Наибольшее значение = {max}");
-
-            Console.ReadLine();
-
-
-        }
-        public static void Main_2()
-        {
-            string[] grades = Console.ReadLine().Split(", ");
-            int count_twos = 0;
-            int count_threes = 0;
-            int count_fives = 0;
-            foreach (string s in grades)
-            {
-                int value = Convert.ToInt32(s);
-                switch (value)
-                {
-                    case 2:
-                        count_twos++;
-                        break;
-                    case 3:
-                        count_threes++;
-                        break;
-                    case 5:
-                        count_fives++;
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            if (count_fives == grades.Length)
-            {
-                // поездка на олимпиаду
-                Console.WriteLine("О, отличник появился! На олимпиаду пойдешь");
-            }
-            else
-            {
-                if (count_twos == 0)
-                {
-                    if (count_threes == 0)
-                    {
-                        //жди полгода
-                        Console.WriteLine("Живи пока, через полгода увидимся");
-                    }
-                    else
-                    {
-                        // прощай стипендия
-                        Console.WriteLine("Прощай стипендия!");
-                    }
-                }
-                else
-                {
-                    // пора в армию
-                    Console.WriteLine("Ну что, студент, пора долг Родине отдать");
-                }
-                Console.ReadLine();
-            }
-        }
-        public static void Main()
-        {
-            string cardNumber = Console.ReadLine();
-            int min = Convert.ToInt32(cardNumber[0].ToString());
-            int max = Convert.ToInt32(cardNumber[0].ToString());
-            char[] ar = cardNumber.ToCharArray();
-
-            for (int index = 0; index < cardNumber.Length; index++)
-            {
-                int value = Convert.ToInt32(cardNumber[index].ToString());
-                if (value < min)
-                    min = value;
-                if (value > max)
-                    max = value;
-            }
-            Array.Reverse(ar);
-            cardNumber = new String(ar);
-            Console.WriteLine($"{cardNumber}{min}{max}");
-
-            Console.WriteLine($"min = {min}; max = {max}");
-
-            Console.ReadLine();
-        }
+        result = new String(Convert.ToChar(symb), Convert.ToInt32(count));
     }
+
+    private static void Opposite(int[] arr)
+    {
+        for (int index = 0; index < arr.Length; index++)
+            arr[index] *= -1;
+    }
+    private static int[] ArraySum(int[] arr1, int[] arr2)
+    {
+        int[] arrResult = new int[arr1.Length > arr2.Length ? arr1.Length : arr2.Length];
+        for (int index = 0; index < arrResult.Length; index++)
+            arrResult[index] = (index < arr1.Length ? arr1[index] : 0) + (index < arr2.Length ? arr2[index] : 0);
+        return arrResult;
+    }
+    private static int GeometricMean(params int[] args)
+    {
+
+        //int counter = args.Length;
+        int mult = 1;
+        foreach (int value in args)
+            mult *= value;
+        double pow_ind = 1.0 / (double)args.Length;
+        return (int)Math.Pow(mult, pow_ind);
+    }
+    private static void ToEven(ref int[] arr)
+    { // это все работает, но попробовать через массив
+      // System.Collections.Generic.List<int> myList = new System.Collections.Generic.List<int>();
+      // foreach (int value in arr)
+      //     if (value % 2 == 0)
+      //         myList.Add(value);
+      // arr = myList.ToArray();
+
+        int[] retArr = new int[arr.Length];
+        int rIndex = 0;
+        for (int index = 0; index < arr.Length; index++)
+            if (arr[index] % 2 == 0)
+            {
+                retArr[rIndex] = arr[index];
+                rIndex++;
+            }
+        arr = retArr.;
+
+
+    }
+
+    public static void Main()
+    {
+        int[] array = { 7, 14, 3, 2, 5, 9, 0, 11, 8 };
+        ToEven(ref array);
+        foreach (var x in array)
+            Console.Write($"{x} ");
+
+        // Console.WriteLine(GeometricMean(5, 25, 1, 0));
+        // Console.WriteLine(GeometricMean(7, 7));
+        // Console.WriteLine(GeometricMean(1, 1, 27));
+        // Console.WriteLine(GeometricMean(2, 4, 1));
+
+        // int[] array1 = { 0, 10, 20, 30, 40, 50 };
+        // int[] array2 = { 1, 2, 3 };
+        // foreach (var x in ArraySum(array1, array2))
+        //     Console.Write($"{x} ");
+
+        // int[] array = { 93, -9, 15, 16, -3, 0, 18 };
+        // Opposite(array);
+        // foreach (var x in array)
+        //     Console.Write($"{x} ");
+
+        // string symbol = "A"; //Console.ReadLine();
+        // string amount = "5"; //Console.ReadLine();
+        // Print(symbol, amount, out string result);
+        // Console.WriteLine(result);
+
+        // string str = "123"; //Console.ReadLine();
+        // int a = int.Parse(str);
+        // double b = (double)a;
+        // char c = str[0];
+        // Console.WriteLine(GetTypeInfo(str));
+        // Console.WriteLine(GetTypeInfo(a));
+        // Console.WriteLine(GetTypeInfo(b));
+        // Console.WriteLine(GetTypeInfo(c));
+
+
+        // это последняя строка, не удалять!!!
+        Console.ReadLine();
+    }
+
 }
+
 
